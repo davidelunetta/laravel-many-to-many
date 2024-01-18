0c72716b749c1cc83b4eac409abe3f4d67b3e687
davidelunetta <davidelunetta.27@gmail.com>
2024-01-18 10:20:10 +0100

    scuffholding

---
+++ database/migrations/2024_01_15_132443_create_project_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->date('start_date');
+            $table->string('image_path',200)->nullable();
+            $table->unsignedBigInteger('type_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('project');
+    }
+};
---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.app')
+@section('content')
+
+<div class="container">
+    <div>
+        <button class="btn btn-primary d-flex align-items-end" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+            <i class="fa-solid fa-bars" style="color: #ffffff;"></i>
+          </button>
+          
+          <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+            <div class="offcanvas-header">
+              <h5 class="offcanvas-title" id="offcanvasExampleLabel">Menù</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+              <div>
+                Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
+              </div>
+              {{-- <div class="dropdown mt-3">
+                <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
+                  Dropdown button
+                </button>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Action</a></li>
+                  <li><a class="dropdown-item" href="#">Another action</a></li>
+                  <li><a class="dropdown-item" href="#">Something else here</a></li>
+                </ul>
+              </div> --}}
+            </div>
+          </div>
+    </div>
+
+    <h1>Lista dei Progetti</h1>
+    <div class="row">
+        @foreach($projects as $project)
+        <div class="col-md-4 mb-3">
+            <div class="card mx-2 d-flex">
+                    @if($project->image_path)
+                        <img src="{{ asset('storage/' . $project->image_path) }}" alt="Immagine del progetto">
+                    @endif
+                <div class="card-body">
+                    <h5 class="card-title mt-1">{{ $project->name }}</h5>
+                    <p class="card-text">{{ $project->description }}</p>
+                    <p class="card-text"><strong>Data di inizio:</strong> {{ $project->start_date }}</p>
+                    <a href="{{ route('admin.projects.show', $project) }}" class="btn btn-primary mt-3">Dettagli</a>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
+    
+    <a href="{{ route('admin.projects.create') }}" class="btn btn-primary">Nuovo Progetto</a>
+    
+</div>
+@endsection
